2b48a03686c52041804721ae4b565fe4a862dbde
wanganqing <wanganqing0502@163.com>
2017-12-28 18:38:32 +0800

    Update mysql-config.js

--- database/mysql-config.js
+++ database/mysql-config.js
@@ -1,13 +1,5 @@
 var connection;
 //正式环境数据
-// connection = {
-//     database: 'first', // 使用哪个数据库
-//     username: 'root', // 用户名
-//     password: 'd1d263ab41', // 口令
-//     host: '119.23.253.140', // 主机名
-//     port: 3306 // 端口号，MySQL默认3306
-// }
-// 
 connection = {
     database: 'waqdb', // 使用哪个数据库
     username: 'root', // 用户名
