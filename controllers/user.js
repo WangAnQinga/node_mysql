555b71b6565f33afc45f8f8cb56574365f10309c
WangAnQinga <wanganqing0502@163.com>
2018-01-03 18:34:31 +0800

    api修改

--- controllers/user.js
+++ controllers/user.js
@@ -47,17 +47,19 @@ module.exports = {
         (async() => {
             var page = parseInt(req.query.page)
             var count = parseInt(req.query.count)
-            var total = await model.user.count();
-            await model.user.findAll({
-                    // where: '', //为空，获取全部，也可以自己添加条件
-                    offset: (page - 1) * count, //开始的数据索引，比如当page=2 时offset=10 ，而pagesize我们定义为10，则现在为索引为10，也就是从第11条开始返回数据条目
-                    limit: count //每页限制返回的数据条数
-                }).then((response) => {
-                    res.json({ code: 200, message: '查询成功', result: response, totalCount: total })
-                }).catch((err) => {
-                    // res.end(err);
-                })
-                // res.json({ code: 200, message: '查询成功', result: lists, totalCount: total })
+
+            await model.user.findAndCount({
+                // where: '', //为空，获取全部，也可以自己添加条件
+                offset: (page - 1) * count, //开始的数据索引，比如当page=2 时offset=10 ，而pagesize我们定义为10，则现在为索引为10，也就是从第11条开始返回数据条目
+                limit: count //每页限制返回的数据条数
+            }).then((response) => {
+                res.json({ code: 200, message: '查询成功', result: response.rows, totalCount: response.count })
+            }).catch((err) => {
+                res.end(err)
+            })
+
+
+
         })()
 
     },
@@ -67,23 +69,21 @@ module.exports = {
             var password = req.body.password;
             var time = Date.parse(new Date()) / 1000;
 
-            if (!userName) {
-                return res.send({ code: 417, message: '用户名必须填！', result: null })
+            if (!userName || !password) {
+                res.send({ code: 417, message: '用户信息必须完整！', result: null })
             }
 
-            if (!password) {
-                return res.send({ code: 417, message: '密码必须填！', result: null })
-            }
 
-            var lists = await model.user.findAll();
 
-            lists.map((item) => {
-                if (item.dataValues.userName == userName) {
-                    res.json({ code: 417, message: '名字已存在!', result: null });
-                    return
+            await model.user.findOne({ where: { "userName": userName } }).then((item) => {
+                if (item) {
+                    res.json({ code: 417, message: '用户名已存在！', result: null })
                 }
-
+            }).catch((err) => {
+                res.end(err)
             })
+
+
 
             bcrypt.hash(password, 10, (err, hash) => {
                 if (err) console.log(err)
